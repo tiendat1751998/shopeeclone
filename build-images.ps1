af5364020c5f943b0a926b9a77e29206417ab16d
datdt <dotiendat1751998@gmail.com>
2026-05-19 18:06:54 +0700

    fix: exclude central proto/shopee directory from being blindly compiled by module local find loop

--- build-images.ps1
+++ build-images.ps1
@@ -140,7 +140,7 @@ if (-not $SkipGo) {
                               "        mkdir -p /proto/catalog/v1 && \`n" +
                               "        protoc --proto_path=/proto/shopee --proto_path=/tmp/validate --proto_path=/usr/include --go_out=/proto --go_opt=module=github.com/shopee-clone/shopee/proto --go-grpc_out=/proto --go-grpc_opt=module=github.com/shopee-clone/shopee/proto /proto/shopee/catalog/v1/catalog.proto || exit 1; \`n" +
                               "    fi && \`n" +
-                              "    find . -name `"*.proto`" -type f | while read f; do \`n" +
+                              "    find . -name `"*.proto`" -type f -not -path `"./proto/shopee/*`" | while read f; do \`n" +
                               "        echo `"Compiling `$f...`" && \`n" +
                               "        protoc --proto_path=. --proto_path=/tmp/validate --proto_path=/usr/include --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative `"`$f`" || exit 1; \`n" +
                               "    done`n" +
--- build-images.sh
+++ build-images.sh
@@ -106,7 +106,7 @@ if [ "$SKIP_GO" = false ]; then
                     print "        mkdir -p /proto/catalog/v1 && \\"
                     print "        protoc --proto_path=/proto/shopee --proto_path=/tmp/validate --proto_path=/usr/include --go_out=/proto --go_opt=module=github.com/shopee-clone/shopee/proto --go-grpc_out=/proto --go-grpc_opt=module=github.com/shopee-clone/shopee/proto /proto/shopee/catalog/v1/catalog.proto || exit 1; \\"
                     print "    fi && \\"
-                    print "    find . -name \"*.proto\" -type f | while read f; do \\"
+                    print "    find . -name \"*.proto\" -type f -not -path \"./proto/shopee/*\" | while read f; do \\"
                     print "        echo \"Compiling $f...\" && \\"
                     print "        protoc --proto_path=. --proto_path=/tmp/validate --proto_path=/usr/include --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative \"$f\" || exit 1; \\"
                     print "    done"
